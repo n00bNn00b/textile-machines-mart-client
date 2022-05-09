d1b056d35d34c46153fabac472695c68b2c42dbb
n00bNn00b <reldewan9@gmail.com>
2022-05-09 19:45:13 +0600

    textile machines' mart client project initial setup

---
+++ src/pages/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="container">
+      <h2>Banner</h2>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import Footer from "../SharedComponents/Footer/Footer";
+import Header from "../SharedComponents/Header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
